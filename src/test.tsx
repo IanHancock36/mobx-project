cad6c440afb53fdb2882eba175eaf74efd401a9b
Ian Hancock <northroadian@gmail.com>
2021-07-01 06:29:42 -0500

    still dependacy issues

--- src/test.tsx
+++ src/test.tsx
@@ -6,6 +6,42 @@ const person = observable({
     firstName: 'MobX',
     lastName: 'Course'
 });
-console.log(person,'our person')
+
+
+class Person {
+    @observable
+    firstName: string ;
+
+    constructor(name: string){
+        this.firstName = name; 
+    }
+    @action
+    updateFirstName(name: string){
+        this.firstName = name;
+
+    }
+
+
+
+
+}
+
+const newPerson = newPerson(name; 'georgy Glexer')
+
+
+
 
 export {}
+
+
+// you must keep variables as const not let. once you call a variable under 'let'
+    // It will lose the mobX functionality. 
+
+// Actions are what we use to update our state 
+    // 3 ways to update state with action 
+        // 1. decorator 
+            // 2. action function 
+                // 3. runInAction function 
+
+    // All updates inside a action are batched 
+        // Actions do not support asunch operations so everything updates after the asynch code will be executed in the next batch. 
